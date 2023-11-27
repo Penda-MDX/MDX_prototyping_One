80c43323b3d1c1e355c696afce5c0ecbc0818ab2
Penda-MDX <p.tomlinson@mdx.ac.uk>
2023-11-27 17:22:53 +0000

    Experiment Balance

---
+++ Experiments/Assets/Scripts/OnTheBar.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnTheBar : MonoBehaviour
+{
+    public Canvas balanceUI;
+    public Slider balanceSlider;
+
+    public float nudgeAmount = 1.0f;
+    public float driftAmount = 0.2f;
+
+    public float balanceNumber = 0;
+    public int balanceRounded;
+
+    public bool inTheZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inTheZone)
+        {
+            if (!balanceUI.isActiveAndEnabled)
+            {
+                balanceUI.enabled = true;
+            }
+            if (balanceNumber > 0) 
+            { 
+                if (balanceNumber < 100.1) 
+                {
+                    balanceNumber += driftAmount;
+                }
+                else
+                {
+                    //fall off right
+                    balanceUI.enabled = false;
+                }
+            }
+
+            if (balanceNumber < 0)
+            {
+                if (balanceNumber > -100.1)
+                {
+                    balanceNumber -= driftAmount;
+                }
+                else
+                {
+                    //fall off left
+                    balanceUI.enabled = false;
+
+                }
+            }
+            if (Input.GetKey("k"))
+            {
+                balanceNumber -= nudgeAmount;
+            }
+            if (Input.GetKey("l"))
+            {
+                balanceNumber += nudgeAmount;
+            }
+
+            balanceSlider.value = balanceNumber;
+
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            inTheZone = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            inTheZone = false;
+        }
+    }
+
+
+}
